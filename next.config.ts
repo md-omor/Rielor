27b68207af705f861281a51bc09dff0e142cff17
Your Name <developeromor@gmail.com>
2026-02-07 23:12:52 +0600

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,19 @@
-
 const nextConfig = {
- serverExternalPackages: [
-  "pdfjs-dist",
-  "puppeteer-core",
-   "pdf-parse",
-],
+  serverExternalPackages: [
+    "puppeteer-core",
+    "@sparticuz/chromium-min",
+    // if you ever use full package:
+    // "@sparticuz/chromium",
+    "pdfjs-dist",
+    "pdf-parse",
+  ],
+
   outputFileTracingIncludes: {
+    "/api/extract-jd": [
+      "./node_modules/puppeteer-core/**",
+      "./node_modules/@sparticuz/chromium-min/**",
+    ],
+
     "/api/upload": [
       "./node_modules/pdf-parse/**",
       "./node_modules/pdfjs-dist/**",
